132108da678363685b67ba8bd974672a4c534c61
jeyson <jeysonrgxd@gmail.com>
2020-11-26 09:00:00 -0500

    echo server

---
+++ node-backend-semi_avanzado/node-web/echo-server.js
@@ -0,0 +1,40 @@
+const { createServer } = require('http')
+const server = createServer()
+
+// el servidor tambien funciona con eventos
+server.on('request', (req, res) => {
+   // verificamos que metodo es
+   if (req.method == 'POST' && req.url == "/echo") {
+
+      let body = []
+      let dias = [
+         'lunes',
+         'martes',
+         'miercoles',
+         'jueves',
+         'viernes',
+         'sabado',
+         'domingo',
+      ]
+
+      req.on('data', chunks =>{
+         body.push(chunks)
+      })
+      .on('end',()=>{
+         let fechaNacimiento = new Date(Buffer.concat(body).toString())
+         let dia = dias[fechaNacimiento.getDay()]
+         res.writeHead(200, { 'Content-Type': 'text/plain' })
+         res.end(`El dia en que naciste es: ${dia}`)
+      })
+
+      
+
+   } else {
+      res.writeHead(400, { 'Content-Type': 'application/json' })
+      res.end(JSON.stringify({ ok: false, mensaje: "No se econtre la pagina" }))
+   }
+})
+
+//levantamos nuestro servidor
+server.listen(9000)
+console.log('Servidor en la url http://localhost:9000')
--- node-backend-semi_avanzado/node-web/server.js
+++ node-backend-semi_avanzado/node-web/server.js
@@ -3,9 +3,10 @@ const server = createServer()
 
 // el servidor tambien funciona con eventos
 server.on('request', (req, res) => {
-   res.status = '200'
-   res.setHeader('Content-type', 'text/html; charset=UTF-8')
-   res.end('<h1>Hola como estas</h1>')
+   // res.status = '200'
+   // res.setHeader('Content-type', 'text/html; charset=UTF-8')
+   res.writeHead(200, { 'Content-type':'text/html; charset=UTF-8'})
+   res.end(JSON.stringify({ ok: true, mensaje: "hola" }))
 })
 
 //levantamos nuestro servidor
@@ -13,3 +14,4 @@ server.listen(9000)
 console.log('Servidor en la url http://localhost:9000')
 
 // ...seguir con el video despues  https://github.com/JasanHdz/backendnodejs/tree/master/notes#arquitectura-orientada-a-eventos
+
